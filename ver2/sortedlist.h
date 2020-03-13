4b25231e8d16cbcdf8327fe6dcd1fc7b6015cf64
Shin Hong <hongshin@handong.edu>
2020-03-14 04:39:13 +0900

    Add sortedlist

---
+++ ver2/sortedlist.h
@@ -0,0 +1,25 @@
+struct sortedlist {
+	int capacity ;
+	int length ;
+	int * elements ;
+} ;
+
+typedef struct sortedlist sortedlist_t ;
+
+sortedlist_t * 
+sortedlist_alloc ()  ;
+
+void
+sortedlist_insert (sortedlist_t * l, int e) ;
+
+int
+sortedlist_retrieve (sortedlist_t * l, int pos, int * e) ;
+
+int 
+sortedlist_length (sortedlist_t * l) ;
+
+int 
+sortedlist_pop (sortedlist_t * l, int * e) ;
+
+int 
+sortedlist_lookup (sortedlist_t * l, int v) ;
