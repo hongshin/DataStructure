f1436d6053dae42d263fabe99abc464d2b0a7fef
Shin Hong <hongshin@handong.edu>
2020-04-17 20:25:08 +0900

    Update knight

--- knight/knight.c
+++ knight/knight.c
@@ -2,15 +2,21 @@
 #include <stdlib.h>
 #include "queue.h"
 
+const int d[8][2] = {{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, 
+					{1, -2}, {1, 2}, {2, -1}, {2, 1}} ;
+
 typedef struct {
 	int x ;
 	int y ;
-	int m ;
-} pos ;
+} pos ; 
 
-int d[8][2] = {{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}} ;
+typedef struct {
+	pos p ;
+	int m ;
+} task ;
 
 int visited[8][8] ;
+pos ancestor[8][8] ;
 
 pos init ;
 pos dest ;
@@ -18,50 +24,67 @@ pos dest ;
 void
 print_element (void * p)
 {
-	pos * elem = (pos *) p ;
-	printf("%d,%d,%d", elem->x, elem->y, elem->m) ;
+	task * elem = (task *) p ;
+	printf("%d,%d,%d", elem->p.x, elem->p.y, elem->m) ;
 }
 
 int
 main () 
 {
 	queue * tasks ;
-	tasks = create_queue(64, sizeof(pos)) ;
 
-	scanf("%d %d", &(init.x), &(init.y)) ;
-	scanf("%d %d", &(dest.x), &(dest.y)) ;
+	tasks = create_queue(64, sizeof(task)) ;
 
-	init.m = 0 ;
-	enqueue(tasks, &init) ;
+	scanf("%d %d", &(init.y), &(init.x)) ;
+	scanf("%d %d", &(dest.y), &(dest.x)) ;
+
+
+	visited[init.y][init.x] = 1 ;
+	ancestor[init.y][init.x] = init ;
+
+	task init_task ;
+	init_task.p = init ;
+	init_task.m = 0 ;
+	enqueue(tasks, &init_task) ;	
 	while (!is_empty(tasks)) {
-		pos curr ;
+		task curr ;
+
 		dequeue(tasks, &curr) ;
-
-		if (curr.x == dest.x && curr.y == dest.y) {
+		if (curr.p.x == dest.x && curr.p.y == dest.y) {
 			printf("%d\n", curr.m) ;
-			return 0 ;
+
+			printf("(%d,%d)", curr.p.y, curr.p.x) ;
+			pos a = curr.p ;
+			while (a.x != ancestor[a.y][a.x].y || a.y != ancestor[a.y][a.x].x) {
+				a = ancestor[a.y][a.x] ;
+				printf("-(%d,%d)", a.y, a.x) ;
+			}
+			printf("\n") ;
+			delete_queue(tasks) ;
+			exit(0) ;
 		}
+		else {
+			int i_d ;
+			for (i_d = 0 ; i_d < 8 ; i_d++) {
+				task next ;
 
-		if (visited[curr.y][curr.x])
-			continue ;
-
-		visited[curr.y][curr.x] = 1 ;
-
-		int i ;
-		for (i = 0 ; i < 8 ; i++) {
-			pos next ;
-			next.x = curr.x + d[i][0] ;
-			next.y = curr.y + d[i][1] ;
-			next.m = curr.m + 1 ;
-
-			if (0 <= next.x && next.x < 8 && 0 <= next.y && next.y < 8) {
-				enqueue(tasks, &next) ;
+				next.p.y = curr.p.y + d[i_d][0] ;
+				next.p.x = curr.p.x + d[i_d][1] ;
+				next.m = curr.m + 1 ;
+				if (0 <= next.p.x && next.p.x < 8) {
+					if (0 <= next.p.y && next.p.y < 8) {
+						if (visited[next.p.y][next.p.x] == 0) {
+							enqueue(tasks, &next) ; 
+							visited[next.p.y][next.p.x] = 1 ;
+							ancestor[next.p.y][next.p.x] = curr.p ;
+						}
+					}
+				}
 			}
 		}
 	}
-
+	// assume is_empty() 
 	printf("unreachable\n") ;
-
 	delete_queue(tasks) ;
 	return 0 ;
 }
